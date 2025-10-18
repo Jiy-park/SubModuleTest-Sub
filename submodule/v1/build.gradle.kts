6466733a098c9a23742da87a624e89735b7485bd
JiY <jiyong3954@gmail.com>
2025-10-18 15:45:38 +0900

    feat: 기존 모듈 제거 및 버전 관리 방식 변경 테스트2

--- submodule/v1/build.gradle.kts
+++ submodule/v1/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.kotlin.android)
+    id("com.android.library") version "8.13.0"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
 }
 
 android {
