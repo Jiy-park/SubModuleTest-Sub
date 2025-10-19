d615cf4a575a66d81a38529d41fc06758ba444be
JiY <jiyong3954@gmail.com>
2025-10-19 16:48:22 +0900

    feat: deprecated AuthRepository, add AuthRepository2

--- auth/src/main/java/com/dd2d/auth/AuthRepository.kt
+++ auth/src/main/java/com/dd2d/auth/AuthRepository.kt
@@ -1,5 +1,10 @@
 package com.dd2d.auth
 
+@Deprecated(
+  message = "use AuthRepository2",
+  replaceWith = ReplaceWith("AuthRepository2", "package com.dd2d.auth.AuthRepository2"),
+  level = DeprecationLevel.WARNING
+)
 interface AuthRepository {
   fun signIn()
   fun signUp()
